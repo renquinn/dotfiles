a337c5952c78dc362dd14c1d3197f34a26be1d81
Ren Quinn <renquinn@cs.utah.edu>
2014-03-23 11:31:34 -0600

    Improve setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,54 @@
 #!/usr/bin/env bash
 
-echo "Installing dotfiles..."
+function help() {
+    echo "usage: ./setup.sh <command>";
+    echo "";
+    echo "Commands:";
+    echo "    install       Create symlinks";
+    echo "    update        Update symlinks";
+    echo "    help          Display this help";
+}
 
 function link() {
     echo -e "\nInstalling $1";
-    ln -s $PWD/$1 $2
+
+    ARGS="-s$3"
+
+    if ! ( ln $ARGS $PWD/$1 $2 ) ; then
+        echo "Try using the update command (see help)"
+    else
+        echo "OK"
+    fi
 }
 
-link bash_profile ~/.bash_profile
-link git-completion.bash ~/.git-completion.bash
-link gitconfig ~/.gitconfig
-link gitignore ~/.gitignore
-link inputrc ~/.inputrc
-link vimrc ~/.vimrc
+function perform() {
+    echo "Installing dotfiles...";
+
+    link bash_profile ~/.bash_profile $1
+    link git-completion.bash ~/.git-completion.bash $1
+    link gitconfig ~/.gitconfig $1
+    link gitignore ~/.gitignore $1
+    link inputrc ~/.inputrc $1
+    link vimrc ~/.vimrc $1
+}
+
+CMD=$1;
+
+case "$CMD" in
+    install)
+        perform;
+        exit $?;
+    ;;
+    update)
+        perform f;
+        exit $?;
+    ;;
+    help)
+        help;
+        exit $?;
+    ;;
+    *)
+        help;
+        exit $?;
+    ;;
+esac
